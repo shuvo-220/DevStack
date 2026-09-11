952bf9c6857d272596e25182557b4f96ff9453bd
shuvo-220 <shuvo.info220@gmail.com>
2026-09-11 16:45:28 +0600

    explore section start

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Explore from './components/Explore'
 
 const App = () => {
   return (
     <div >
       <Navbar />
       <Hero />
+       <Explore />
       <Footer />
+     
     </div>
   )
 }
---
+++ src/components/Explore.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Explore = () => {
+  return (
+    <div className='py-10 mx-10'>
+        <div>
+            <h1 className='text-4xl font-bold' >Explore the <span className='bg-gradient-to-r from-pink-500 to-pink-600 bg-clip-text text-transparent'>technologies</span></h1>
+            <p className='py-3 text-gray-500'>Pick one technology per category to build your ideal stack.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Explore
